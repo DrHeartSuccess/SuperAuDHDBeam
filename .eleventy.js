61469bd98c76ae407e8fee43a5589f6f1fc818e1
Dr Heart Success💫 <drheartsuccess021@gmail.com>
2025-04-22 04:49:46 +1200

    Updates

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+
+const now = String(Date.now());
+
+export default function (eleventyConfig) {
+  eleventyConfig.addWatchTarget('./styles/tailwind.config.js');
+  eleventyConfig.addWatchTarget('./styles/tailwind.css');
+
+  eleventyConfig.addPassthroughCopy({ './_tmp/style.css': './style.css' });
+  eleventyConfig.addPassthroughCopy("images");
+
+  eleventyConfig.addShortcode('version', function () {
+    return now;
+  });
+
+  eleventyConfig.addCollection("comicPages", function(collectionApi){
+    return collectionApi.getFilteredByTag("comic").sort((a, b) => a.date - b.date);
+  });
+};
+
