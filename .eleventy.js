e3986e08376ff2585a8cc09875d9fff209d6e037
Dr Heart Success💫 <drheartsuccess021@gmail.com>
2025-04-21 17:34:48 +1200

    enviroment setup

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+const now = String(Date.now());
+
+export default function (eleventyConfig) {
+  eleventyConfig.addWatchTarget('./styles/tailwind.config.js');
+  eleventyConfig.addWatchTarget('./styles/tailwind.css');
+
+  eleventyConfig.addPassthroughCopy({ './_tmp/style.css': './style.css' });
+
+  eleventyConfig.addShortcode('version', function () {
+    return now;
+  });
+}
+
